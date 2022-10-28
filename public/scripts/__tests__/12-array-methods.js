d334faf1fce1b39f72ad73902289bdf76fcf603f
loco9939 <kls9939@naver.com>
2022-10-28 13:04:54 +0900

    feat: Create test code 12-array-methods

--- public/scripts/__tests__/12-array-methods.js
+++ public/scripts/__tests__/12-array-methods.js
@@ -1,6 +1,6 @@
 /* eslint-disable jest/expect-expect */
 import { fetchBooks } from '../../utils/fetchBooks';
-import { currencyKR } from '../../utils/index';
+import { currencyKR as currency } from '../../utils/index';
 
 // -----------------------------------------------------------------------------
 // TODO & TEST
@@ -18,69 +18,111 @@ async function getBooks() {
   try {
     let data = await fetchBooks();
     return new Promise((res,rej) => {
-      res(data.map(v => ({...v,price:currencyKR(v.price)})))
+      res(data.map(v => ({...v,price:currency(v.price)})))
     });
-  } catch(e) {
-    console.error(e.message);
+  } catch {
+    // 통신에 실패한 경우 오류 메시지를 throw 합니다.
+    throw new Error('통신에 실패했습니다.')
   }
-  // 통신에 실패한 경우 오류 메시지를 throw 합니다.
 }
 
-test.only('도서 목록 가져오기', async () => {
+test('도서 목록 가져오기', async () => {
   // 테스트 코드 작성
   const data = await getBooks();
-  data.forEach(v => {
-    expect(v.price).toBe(v.price)
-  })
+  expect(data).toHaveLength(7);
 });
 
 // -----------------------------------------------------------------------------
 // 제목으로 도서 목록 검색
 // -----------------------------------------------------------------------------
 
-function searchBooksByTitle(query) {
+async function searchBooksByTitle(query) {
   // Async 함수를 활용해보세요.
   // 모든 도서 목록을 가져온 후, 전달 받은 쿼리와 도서 제목을 포함하는 결과를 반환합니다.
   // 통신에 실패한 경우 오류 메시지를 throw 합니다.
+  try {
+    let data = await getBooks();
+    data = data.filter(v => v.title.includes(query));
+    return data;
+  } catch {
+    throw new Error('통신에 실패하였습니다.')
+  }
 }
 
-test('제목으로 도서 목록 검색', () => {
+test('제목으로 도서 목록 검색', async () => {
   // 테스트 코드 작성
-  searchBooksByTitle('파랑');
+  expect(await searchBooksByTitle('파랑')).toHaveLength(3);
 });
 
 // -----------------------------------------------------------------------------
 // 제목 또는 저자로 도서 찾기
 // -----------------------------------------------------------------------------
 
-function findBook(query, by = 'title') {
+async function findBook(query, by = 'title') {
   // Promise 또는 Async 함수 중 1가지 방법을 선택합니다.
   // 모든 도서 목록을 가져온 후, 전달 받은 매개변수를 사용해 결과(1개)를 반환합니다.
   // 통신에 실패한 경우 오류 메시지를 throw 합니다.
+  try {
+    const data = await getBooks();
+    return data.find(v => v[by].includes(query));
+  } catch {
+    throw new Error('통신에 실패했습니다.')
+  }
 }
 
-test('제목으로 도서 찾기', () => {
-  // 테스트 코드 작성
-  findBook('연애');
-});
-
-test('저자로 도서 찾기', () => {
-  // 테스트 코드 작성
-  findBook('정이담', 'author');
-});
+describe('제목 또는 저자명으로 도서 찾기', () => {
+  test('제목으로 도서 찾기', async () => {
+    // 테스트 코드 작성
+    const expected = {
+      "id": "ISBN-9791166113239",
+      "title": "연애의 행방",
+      "author": "히가시노 게이고",
+      "image": {
+        "src": "/assets/9791166113239.jpeg",
+        "width": 458
+      },
+      "price": "13,320원"
+    }
+    expect(await findBook('연애')).toStrictEqual(expected);
+  });
+  
+  test('저자로 도서 찾기', async () => {
+    // 테스트 코드 작성
+    const expected = {
+      "id": "ISBN-9791158888367",
+      "title": "불온한 파랑",
+      "author": "정이담",
+      "image": {
+        "src": "/assets/9791158888367.jpeg",
+        "width": 458
+      },
+      "price": "12,420원"
+    };
+    expect(await findBook('정이담', 'author')).toStrictEqual(expected);
+  });
+})
 
 // -----------------------------------------------------------------------------
 // 도서 가격 총합 구하기
 // -----------------------------------------------------------------------------
 
-function calcurateBooksPrice(query) {
+async function calcurateBooksPrice(query) {
+    try {
+      let data = await fetchBooks();
+      data = data.filter(v => v.title.includes(query)).map(v => v.price);
+      return currency(data.reduce((acc,cur) => acc+cur,0));
+    } catch {
+      throw new Error('통신에 실패했습니다.');
+    }
   // Promise 또는 Async 함수 중 1가지 방법을 선택합니다.
   // searchBooksByTitle() 함수에 전달 받은 매개변수를 사용해 도서 목록을 가져옵니다.
   // 도서 목록을 순회해 가격을 모두 더한 결과 값을 반환합니다.
   // 통신에 실패한 경우 오류 메시지를 throw 합니다.
 }
 
-test('도서 가격 총합 구하기', async () => {
+test.only('도서 가격 총합 구하기', async () => {
   // 테스트 코드 작성
-  calcurateBooksPrice('파랑');
+  const expected = '40,140원';
+  const result = await calcurateBooksPrice('파랑')
+  expect(result).toBe(expected);
 });
